2d29ecba5a07acd8ac0b7db2cc3b095418ab6537
Ebru Kaya <ebru@ebrukaya.me>
2023-05-22 20:03:20 +0200

    percentage adjustments reverted

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -10,23 +10,6 @@ interface PieChartProps {
 }
 
 const PieChart: React.FC<PieChartProps> = ({ data }) => {
-  const adjustPercentages = (rawPercentages: any) => {
-    let sum = 0;
-    let floorSums = 0;
-    const result = rawPercentages.map((value: any) => {
-      const floorValue = Math.floor(value);
-      sum += value;
-      floorSums += floorValue;
-      return floorValue;
-    });
-    let roundError = Math.round(sum) - floorSums;
-    for (let i = 0; i < result.length && roundError > 0; i++) {
-      result[i]++;
-      roundError--;
-    }
-    return result;
-  }
-
   return (
     <div style={{
       height: 350,
@@ -49,9 +32,7 @@ const PieChart: React.FC<PieChartProps> = ({ data }) => {
             datalabels: {
               formatter: (value: any, context: any) => {
                 const total = context.chart.data.datasets[0].data.reduce((a: number, b: number) => a + b, 0);
-                const rawPercentages = context.chart.data.datasets[0].data.map((v: number) => (v / total) * 100);
-                const adjustedPercentages = adjustPercentages(rawPercentages);
-                const percentage = adjustedPercentages[context.dataIndex];
+                const percentage = Math.round((value / total) * 100);
                 return `${percentage}%`;
               },
               color: '#fff',
